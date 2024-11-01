aaecea1c19403ef2cd8a169af978275268f7227b
Yael <yael.rodriguez@iteso.mx>
2024-10-31 19:33:41 -0600

    Empezando ando

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import path from "path";
+import { Server } from 'socket.io' 
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use('', express.static(path.join(__dirname, '..', 'public')));
+
+app.get('', (req, res) => {
+    res.send('Api works!');
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+app.get('/chat/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'chat.html'));
+});
+
+const server = app.listen(port, () => {
+    console.log(`Puerto ${port}`)
+})
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('Se conecto al cliente');
+
+    socket.on('joinRoom', (id) => {
+        socket.join('room-' + id)
+    })
+
+    socket.on('sendMessage', (data) => {
+        console.log(data);
+
+        socket.to('room-' + data.room).emit('getMessage', data)
+        //socket.broadcast.emit('getMessage', data);  //Todos menos yo
+        //socket.to(data.room).emit(data)   Todos menos yo y room
+        //io.emit(data)     Todos incluyendome
+    })
+})
+
+
